ae2373e1aebeaf1bf5580cdf3dff2d0c1ce33e3e
Nguyen Hoang Phu <108948401+1natto@users.noreply.github.com>
2024-02-29 22:47:37 +0700

    asd

--- app.py
+++ app.py
@@ -10,29 +10,14 @@ def index():
 @app.route("/draft1/", methods=['GET'])
 def submit():
     return render_template("submit.html")
-@app.route("/draft2/", methods=['GET'])
-def ny2():
-    return render_template("ny2.html")
-@app.route("/ny3/", methods=['GET'])
-def ny3():
-    return render_template("ny3.html")
-@app.route("/ny4/", methods=['GET'])
-def ny4():
-    return render_template("ny4.html")
-@app.route("/ny5/", methods=['GET'])
-def ny5():
-    return render_template("ny5.html")
-@app.route("/ny6/", methods=['GET'])
-def ny6():
-    return render_template("ny6.html")
 
-#-----------------(thaophuongne1)
-@app.route("/dGhhb3BodW9uZ25lMTM=/", methods=['GET'])
-def thaophuongne1():
-    return render_template("thaophuongne1.html")
-@app.route("/dGhhb3BodW9uZ25lMTM=1/", methods=['GET'])
-def thaophuongne2():
-    return render_template("thaophuongne2.html")
+#-----------------(vugiahung)
+@app.route("/vgh1", methods=['GET', 'POST'])
+def vgh1():
+    return render_template("/vugiahung/vgh1.html")
+@app.route("/vgh2/", methods=['GET'])
+def vgh2():
+    return render_template("/vugiahung/vgh2.html")
 
 if __name__ == '__main__':
    app.run()
