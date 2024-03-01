084b096123225946f6ee9cd4c57a3896a6cbd0b4
Nguyen Hoang Phu <108948401+1natto@users.noreply.github.com>
2024-03-02 01:04:15 +0700

    tap

--- app.py
+++ app.py
@@ -29,11 +29,81 @@ def namk2():
 
 #---------Trần Nhân Nhật Minh
 
+#1
+@app.route("/dG5uaDE=/", methods=['GET'])
+def tnnh1():
+    return render_template("tnnh1.html")
+@app.route("/dG5uaDE=2/", methods=['GET'])
+def tnnh2():
+    return render_template("tnnh2.html")
+
+#2
+@app.route("/dG5uaDE=3/", methods=['GET'])
+def tnnh3():
+    return render_template("tnnh3.html")
+@app.route("/dG5uaDE=4/", methods=['GET'])
+def tnnh4():
+    return render_template("tnnh4.html")
+
+#3
+@app.route("/dG5uaDE=5/", methods=['GET'])
+def tnnh5():
+    return render_template("tnnh5.html")
+@app.route("/dG5uaDE=6/", methods=['GET'])
+def tnnh6():
+    return render_template("tnnh6.html")
+
+#4
+@app.route("/dG5uaDE=7/", methods=['GET'])
+def tnnh7():
+    return render_template("tnnh7.html")
+@app.route("/dG5uaDE=8/", methods=['GET'])
+def tnnh8():
+    return render_template("tnnh8.html")
+
+#5
+@app.route("/dG5uaDE=9/", methods=['GET'])
+def tnnh9():
+    return render_template("tnnh9.html")
+@app.route("/dG5uaDE=10/", methods=['GET'])
+def tnnh10():
+    return render_template("tnnh10.html")
+
+#6
+@app.route("/dG5uaDE=11/", methods=['GET'])
+def tnnh11():
+    return render_template("tnnh11.html")
+@app.route("/dG5uaDE=12/", methods=['GET'])
+def tnnh12():
+    return render_template("tnnh12.html")
 
 
+#7
+@app.route("/dG5uaDE=13/", methods=['GET'])
+def tnnh13():
+    return render_template("tnnh13.html")
+@app.route("/dG5uaDE=14/", methods=['GET'])
+def tnnh14():
+    return render_template("tnnh14.html")
+
+#8
+
+@app.route("/dG5uaDE=15/", methods=['GET'])
+def tnnh15():
+    return render_template("tnnh15.html")
+@app.route("/dG5uaDE=16/", methods=['GET'])
+def tnnh16():
+    return render_template("tnnh16.html")
 
 
+#----------tamanhpham
 
+@app.route("/dGFwMQ==/", methods=['GET'])
+def tap1():
+    return render_template("tap1.html")
+@app.route("/dGFwMQ==2/", methods=['GET'])
+def tap2():
+    return render_template("tap2.html")
 
 
 if __name__ == '__main__':
