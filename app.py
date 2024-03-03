9aedeed147047043f7dbb2efc09f4bac1cbf78e4
minhlol <mdinhthe@gmail.com>
2024-03-04 05:04:27 +0700

    omit 31 + 37 done

--- app.py
+++ app.py
@@ -302,6 +302,12 @@ def tnpl362():
     return render_template("tnpl362.html")
 
 #37
+@app.route("/dnRrYw==/", methods=['GET'])
+def vtkc1():
+    return render_template("vtkc1.html")
+@app.route("/dnRrYw==2/", methods=['GET'])
+def vtkc2():
+    return render_template("vtkc2.html")
 
 #38
 @app.route("/dmJoMzg=/", methods=['GET'])
