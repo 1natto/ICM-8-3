206193c031678446f25c45056191ff88c5378f64
minhlol <mdinhthe@gmail.com>
2024-03-08 12:46:44 +0700

    42 link routing test

--- app.py
+++ app.py
@@ -342,10 +342,10 @@ def ttl412():
     return render_template("ttl412.html")
 
 #42
-@app.route("/dHR0bDQy/", methods=['GET'])
+@app.route("/42/", methods=['GET'])
 def ttl42():
     return render_template("ttl42.html")
-@app.route("/dHR0bDQy==/", methods=['GET'])
+@app.route("/42-/", methods=['GET'])
 def ttl422():
     return render_template("ttl422.html")
 
